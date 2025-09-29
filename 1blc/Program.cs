f054d85f430eb56bb9bf856e2a6ef020bf26ac15
Matthias Henning <henning@geomarketing.de>
2025-09-29 11:50:34 +0200

    weiter

--- 1blc/Program.cs
+++ 1blc/Program.cs
@@ -1,12 +1,18 @@
 ﻿using _1blc;
 using BenchmarkDotNet.Running;
+using System.Diagnostics;
 
-var summary = BenchmarkRunner.Run<ToBench>();
-//var dummy = new ToBench();
+//var summary = BenchmarkRunner.Run<ToBench>();
+var dummy = new ToBench();
 ////
 ////
-//dummy.DoItFloat();
-//dummy.DoItDouble();
+
+
+var stopwatch = Stopwatch.StartNew();
+dummy.DoItFloat();
+stopwatch.Stop();
+
+Console.WriteLine($"DoItFloat runtime: {stopwatch.ElapsedMilliseconds} ms");//dummy.DoItDouble();
 
 //	const string path = @"C:\Users\henning\source\1brc\data\measurements.txt";
 //	var part = new FilePart { FileName = path, Start = 0, End = 700 };
